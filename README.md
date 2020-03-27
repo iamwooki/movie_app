61bce54c75881c9afa291b97b86ac471160717ed
iamwooki <hwhong.korea@gmail.com>
2020-03-28 01:28:22 +0900

    added

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ React JS Fundamentals Course
 npm install npx -g
 npx create-react-app movie_app
 ```
+
+### jsx
+- react application은 하나의 component만 렌더링함 따라서 모든 것은 application안에 들어가야하고 app안에는 많은 components를 넣을 수 있음
+- component에 정보를 보낼 수 있음, react가 멋진 이유는 재사용 가능한 component를 만들 수 있다는 것
--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types";
 
+function Movie({id, name, picture}){
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} />
+    </div>
+  );
+}
+
+const movieILike = [
+  {
+    id:1,
+    name: "Titanic",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/RMS_Titanic_3.jpg/600px-RMS_Titanic_3.jpg",
+    ratings:5.0
+  },
+  {
+    id:2,
+    name: "Harry Porter",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/RMS_Titanic_3.jpg/600px-RMS_Titanic_3.jpg",
+      ratings:5.0
+  },
+  {
+    id:3,
+    name: "Avatar",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/RMS_Titanic_3.jpg/600px-RMS_Titanic_3.jpg",
+      ratings:5.0
+  },
+  {
+    id:4,
+    name: "Avengers",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/RMS_Titanic_3.jpg/600px-RMS_Titanic_3.jpg",
+      ratings:5.0
+  }
+];
+
+Movie.PropTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  ratings: PropTypes.string.isRequired
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      {movieILike.map(movie =>(
+        <Movie key={movie.id} name={movie.name} picture={movie.image} /> 
+        ))}
+      </div>
   );
 }
 
