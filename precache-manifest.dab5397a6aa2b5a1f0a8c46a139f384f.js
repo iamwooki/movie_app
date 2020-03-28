15dc5720912bdc46f9a6ad02e7ce983fe8230ff6
iamwooki <hwhong.korea@gmail.com>
2020-03-28 21:09:36 +0900

    Updates

---
+++ precache-manifest.dab5397a6aa2b5a1f0a8c46a139f384f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0685a0837313d332705ddf8f9f354cc0",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "6dd61c693d96d6baabd0",
+    "url": "/movie_app/static/css/main.a1347d41.chunk.css"
+  },
+  {
+    "revision": "697e48abbd7f0ba75f99",
+    "url": "/movie_app/static/js/2.75f33243.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.75f33243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dd61c693d96d6baabd0",
+    "url": "/movie_app/static/js/main.1854e65b.chunk.js"
+  },
+  {
+    "revision": "77df4e0b724824eca1cd",
+    "url": "/movie_app/static/js/runtime-main.0b3e8895.js"
+  }
+]);
